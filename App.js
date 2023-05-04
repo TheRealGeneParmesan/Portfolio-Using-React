b743df078cd8a41f48a08ac9ef53eebf9a010e72
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-03 19:51:09 -0700

    added app.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <main>
+            <AboutMe />
+            <Portfolio />
+            <Skills />
+            <Resume />
+            <Contact />
+        </main>
+    );
+}
