6539d6932bfa453d0a66cbc53226b8207973510a
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-06 15:26:33 -0700

    changed file structure to make app work. added aboutme

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import Header from "./Components/Header";
+// import Navigation from "./Components/Navigation";
+import About from "./Components/AboutMe";
+// import Portfolio from "./Components/Portfolio";
+// import Project from "./Components/Project";
+// import Resume from "./Components/MyResume";
+// import Contact from "./Components/Contact";
+// import Footer from "./Components/Footer";
+
+function App() {
+    return (
+        <main>
+            {/* <Header /> */}
+            {/* <Navigation /> */}
+            <About />
+            {/* <Portfolio />  */}
+            {/* <Project /> */}
+            {/* <Resume /> */}
+            {/* <Contact /> */}
+            {/* <Footer /> */}
+        </main>
+    );
+}
+
+export default App;
