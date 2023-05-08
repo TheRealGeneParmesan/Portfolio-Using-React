83aab700699d7545cea2a3d1cb0560e193e2026a
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-07 17:47:56 -0700

    Created the header

---
+++ my-portfolio/src/Components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function Header() {
+    const headerStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '1rem',
+        backgroundColor: '#95B8D1',
+        color: '#fff'
+    };
+
+    const logoStyle = {
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+        textDecoration: 'none',
+        color: '#fff'
+    };
+
+    const navStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        fontWeight: 'bold'
+    };
+
+    const linkStyle = {
+        marginLeft: '3rem',
+        textDecoration: 'none',
+        color: '#fff'
+    };
+
+    return (
+        <header style={headerStyle}>
+            <a href="#" style={logoStyle}>My Portfolio</a>
+            <nav style={navStyle}>
+                <a href="#" style={linkStyle}>Home</a>
+                <a href="#" style={linkStyle}>About</a>
+                <a href="#" style={linkStyle}>Contact</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
