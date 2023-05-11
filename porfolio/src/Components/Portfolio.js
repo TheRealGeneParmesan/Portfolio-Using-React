2d276f9aaad3a43997b0914063454a6aaacaf1a3
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-11 16:41:11 -0700

    Added github links & icons to projects & titles

--- porfolio/src/Components/Portfolio.js
+++ porfolio/src/Components/Portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { projects } from './Project';
+import { BsGithub } from "react-icons/bs";
 
 function Portfolio() {
     const projectPhotos = {
@@ -11,42 +12,58 @@ function Portfolio() {
     };
 
     const projectTitle = {
-        fontSize: '1.3rem',
+        fontSize: '3.5rem',
         color: '#9C95DC',
     }
 
+    const projectDescription = {
+        fontSize: '1.5rem',
+        color: '#9C95DC',
+        marginBottom: '3rem'
+    }
+
+    const projectLogo = {
+        textDecoration: 'none',
+        color: '#9C95DC',
+    };
+
     return (
-        <section id="projects" style={projectTitle}>
+        <section id="projects">
             <div className="container px-5 py-10 mx-auto text-center lg:px-40">
                 <div className="flex flex-col w-full mb-20">
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4">
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4" style={projectTitle}>
                         My Projects
                     </h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                        Here are some of the projects I have worked on. Click on the images to be taken to the deployed application.
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base" style={projectDescription}>
+                        Here are several projects I have worked on in the last few months.
+                        <br></br>
+                        <br></br>
+                        Click on the images to be taken to the deployed application or the Github icon to be taken to the repository.
                     </p>
                 </div>
-                <div className="flex flex-wrap flew-rox -m-4">
+                <div className="flex flex-wrap flew-rox -m-4" style={projectTitle}>
                     {projects.map((project, index) => (
-                        <a href={project.link} key={project.image} className="sm:w-1/2 w-100 p-4">
+                        <div key={project.image} className="sm:w-1/2 w-100 p-4">
+                            <h3 style={{ marginBottom: '0.5rem' }}>{project.title}</h3>
                             <div
-                                className={`flex relative ${index % 3 === 0 ? 'justify-start' : index % 3 === 2 ? 'justify-end' : ''
-                                    }`}
+                                className={`flex relative ${index % 3 === 0 ? 'justify-start' : index % 3 === 2 ? 'justify-end' : ''}`}
                                 style={{ flexDirection: 'column' }}
                             >
-                                <img
-                                    alt="gallery"
-                                    src={project.image}
-                                    style={projectPhotos}
-                                    className="w-1/3"
-                                />
-                                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100" style={{ height: '10rem', position: 'absolute', bottom: '-10rem' }}>
-                                    <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                                        {project.description}
-                                    </h2>
+                                <a href={project.link} target="_blank" rel="noopener noreferrer">
+                                    <img
+                                        alt="gallery"
+                                        src={project.image}
+                                        style={projectPhotos}
+                                        className="w-1/3"
+                                    />
+                                </a>
+                                <div style={{ marginTop: '1rem' }}>
+                                    <a href={project.git} target="_blank" rel="noopener noreferrer">
+                                        <BsGithub style={projectLogo} />
+                                    </a>
                                 </div>
                             </div>
-                        </a>
+                        </div>
                     ))}
                 </div>
             </div>
--- porfolio/src/Components/Project.js
+++ porfolio/src/Components/Project.js
@@ -7,48 +7,53 @@ import WeatherDashboard from "../images/weatherdashboard.png";
 
 export const projects = [
     {
-        title: "Musical Chairs",
+        title: "Musical Chairs: A Full Stack Musical Social Media App",
         description: "Social media app to share music with friends using Spotify's API",
         image: MusicalChairs,
-        link: "https://musical-chairs.herokuapp.com/login"
+        link: "https://musical-chairs.herokuapp.com/login",
+        git: "https://github.com/TheRealGeneParmesan/Musical-Chairs-Full-Stack-App"
     },
 
     {
-        title: "Good Greens",
+        title: "Good Greens: A Front End Vegan and Vegetarian Restaurant Finder",
         description: "Application to find vegan and vegetarian restaurants in your city and you can create your own favorites list",
         image: GoodGreens,
-        link: "https://www.togoodgreens.com/"
+        link: "https://www.togoodgreens.com/",
+        git: "https://github.com/shavanapuchta/good-greens-project-1"
 
     },
 
     {
-        title: "Note Taker",
+        title: "Note Taker: A Full Stack Note Taking Application",
         description: "Application to create, save, and delete notes",
         image: NoteTaker,
-        link: "https://express-js-notetaker.herokuapp.com/"
+        link: "https://express-js-notetaker.herokuapp.com/",
+        git: "https://github.com/TheRealGeneParmesan/Notetaker-Using-Express.js"
     },
 
     {
-        title: "Text Editor",
+        title: "Text Editor: A Full Stack PWA Text Editor",
         subtitle: "Full Stack PWA editor using Javascript, CSS, HTML, Node & Express",
         description: "A text editor that ca be used offline and meets PWA criteria. ",
         image: TextEditor,
-        link: "https://jeremys-pwa-text-editor.herokuapp.com/"
+        link: "https://jeremys-pwa-text-editor.herokuapp.com/",
+        git: "https://github.com/TheRealGeneParmesan/PWA-Text-Editor"
     },
 
     {
-        title: "Work Day Scheduler",
+        title: "Work Day Planner: A Front End Daily Schedule Application",
         description: "Application to create a daily schedule",
         image: WorkDayScheduler,
-        link: "https://therealgeneparmesan.github.io/Work-Day-Scheduler/"
-
+        link: "https://therealgeneparmesan.github.io/Work-Day-Scheduler/",
+        git: "https://github.com/TheRealGeneParmesan/Work-Day-Scheduler"
     },
 
     {
-        title: "Weather Dashboard",
+        title: "Weather Dashboard: A Front End Weather Application",
         description: "Application to find the weather in any city",
         image: WeatherDashboard,
-        link: "https://therealgeneparmesan.github.io/Weather_Dashboard/"
+        link: "https://therealgeneparmesan.github.io/Weather_Dashboard/",
+        git: "https://github.com/TheRealGeneParmesan/Weather_Dashboard"
     }
 
 
