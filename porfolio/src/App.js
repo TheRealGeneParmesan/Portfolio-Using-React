6d52be3a1c553a9d5dc7643070b398688db7737d
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-05-11 15:51:39 -0700

    imported CSS into APP.JS

--- porfolio/src/App.js
+++ porfolio/src/App.js
@@ -4,24 +4,18 @@ import Introduction from "./Components/Intro";
 import About from "./Components/About";
 import Portfolio from "./Components/Portfolio";
 import Contact from "./Components/Contact";
-// import Portfolio from "./Components/Portfolio";
 // import Project from "./Components/Project";
 import Footer from "./Components/Footer";
+import "./App.css";
 
 function App() {
-    const aboutRef = useRef(null);
-    const portfolioRef = useRef(null);
-
-    function scrollTo(ref) {
-        ref.current.scrollIntoView({ behavior: 'smooth' });
-    }
 
     return (
         <main>
-            <Header scrollTo={scrollTo} aboutRef={aboutRef} portfolioRef={portfolioRef} />
+            <Header />
             <Introduction />
-            <About aboutRef={aboutRef} />
-            <Portfolio portfolioRef={portfolioRef} />
+            <About />
+            <Portfolio />
             <Contact />
             <Footer />
         </main>
