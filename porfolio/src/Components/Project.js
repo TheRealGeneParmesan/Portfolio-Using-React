58e19ff2cbc367c9b4e587a39cfdf8882ee25437
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-06-05 19:47:46 -0700

    updated app sin portfolio

--- porfolio/src/Components/Project.js
+++ porfolio/src/Components/Project.js
@@ -1,11 +1,21 @@
 import MusicalChairs from "../images/MusicalChairs.png"
 import GoodGreens from "../images/Goodgreen.png";
-import NoteTaker from "../images/Notetaker.png";
-import TextEditor from "../images/Texteditor.png";
+import BookSearch from "../images/BookSearch.png";
+import Fitness from "../images/FitnRun.png"
 import WorkDayScheduler from "../images/WorkdayScheduler.png";
 import WeatherDashboard from "../images/weatherdashboard.png";
 
 export const projects = [
+
+    {
+        title: "Fit N Run: A Full Stack Fitness App",
+        description: "Application using the MERN stack to log your fitness goals and find the best workouts to target certain muscles",
+        image: Fitness,
+        link: "https://fit-n-run.herokuapp.com/",
+        git: "https://github.com/TheRealGeneParmesan/Fit-N-Run"
+    },
+
+
     {
         title: "Musical Chairs: A Full Stack Musical Social Media App",
         description: "Social media app to share music with friends using Spotify's API",
@@ -15,29 +25,20 @@ export const projects = [
     },
 
     {
+        title: "Book Search Engine",
+        description: "A full stack app using MERN allowing a user to search for books and save books",
+        image: BookSearch,
+        link: "https://book-search-engine-with-mern.herokuapp.com/",
+        git: "https://github.com/TheRealGeneParmesan/Book-Search-Engine-MERN"
+    },
+
+    {
         title: "Good Greens: A Front End Vegan and Vegetarian Restaurant Finder",
         description: "Application to find vegan and vegetarian restaurants in your city and you can create your own favorites list",
         image: GoodGreens,
         link: "https://www.togoodgreens.com/",
         git: "https://github.com/shavanapuchta/good-greens-project-1"
 
-    },
-
-    {
-        title: "Note Taker: A Full Stack Note Taking Application",
-        description: "Application to create, save, and delete notes",
-        image: NoteTaker,
-        link: "https://express-js-notetaker.herokuapp.com/",
-        git: "https://github.com/TheRealGeneParmesan/Notetaker-Using-Express.js"
-    },
-
-    {
-        title: "Text Editor: A Full Stack PWA Text Editor",
-        subtitle: "Full Stack PWA editor using Javascript, CSS, HTML, Node & Express",
-        description: "A text editor that ca be used offline and meets PWA criteria. ",
-        image: TextEditor,
-        link: "https://jeremys-pwa-text-editor.herokuapp.com/",
-        git: "https://github.com/TheRealGeneParmesan/PWA-Text-Editor"
     },
 
     {
